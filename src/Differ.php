10f315e789acae3c4e8485141dde1a0f975614b2
Stenido.S <os8333324@gmail.com>
2023-12-27 15:20:59 +0300

    edit

--- src/Differ.php
+++ src/Differ.php
@@ -2,7 +2,7 @@
 
 namespace Differ\Differ;
 
-function gendiff(string $path1, string $path2): string
+function genDiff(string $path1, string $path2): string
 {
     return 'Hello, ' . $path1 . ', ' . $path2 . '!';
 }
