a8b065fed20fe9f888f9caf350b8b96e14a483cb
Stenido.S <os8333324@gmail.com>
2023-12-27 14:48:52 +0300

    modified

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -9,7 +9,7 @@ final class DifferTest extends TestCase
 {
     public function testDiffer(): void
     {
-        
+
         $greeting = genDiffer('Alice', 'Boob');
 
         $this->assertSame('Hello, Alice, Boob!', $greeting);
