464ae1a171a7b2ed0512f151e94afb77fbeeb8bf
Stenido.S <os8333324@gmail.com>
2024-01-07 12:58:56 +0300

    добавил тесты для новой функции Stringify()

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use function Differ\Differ\stringify;
 use function Differ\Differ\genDiff;
 use PHPUnit\Framework\TestCase;
 
@@ -18,6 +19,30 @@ final class DifferTest extends TestCase
         $message = "actual value for function test2() is not equals to expected"; // в сообщении можно определить конкретно данный тест для удобства работы с ошибками 
         
         $this->assertSame($expected, $actual, $message);
+    }
+    
+    // Проверка на примитивных типах
+    public function testPrimitiveTypes()
+    {
+        $this->assertSame('123', stringify(123));
+        $this->assertSame('"abc"', stringify('abc'));
+        $this->assertSame('true', stringify(true));
+    }
+
+    // Проверка на "плоских" данных
+    public function testFlatData()
+    {
+        $flatData = array('key1' => 'value1', 'key2' => 'value2');
+        $expectedResult = "{\n key1: value1\n key2: value2\n}";
+        $this->assertSame($expectedResult, stringify($flatData));
+    }
+
+    // Проверка на "вложенных" данных
+    public function testNestedData()
+    {
+        $nestedData = array('key1' => array('nestedKey1' => 'nestedValue1'), 'key2' => 'value2');
+        $expectedResult = "{\n key1: {\"nestedKey1\":\"nestedValue1\"}\n key2: value2\n}";
+        $this->assertSame($expectedResult, stringify($nestedData));
     }
     
     //определена как приватная, чтобы ограничить ее доступность только внутри того класса или объекта, в котором она объявлена.
