7e021d5bc54df57436080152e7f2e3f4731c753b
Stenido.S <os8333324@gmail.com>
2024-01-08 12:44:18 +0300

    много чего2))

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,7 +6,7 @@ use Symfony\Component\Yaml\Yaml;
 
 function parseYaml($fileName)
 {
-    $value = Yaml::parseFile(__DIR__ . "tests/fixtures/" . $fileName);
+    $value = Yaml::parseFile("/home/stenido/MyProjects/lvl2.2/tests/fixtures/" . $fileName);
     return $value;
 }
 
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use function Differ\Differ\stringify;
+
 use function Differ\Differ\genDiff;
 use function Differ\Parsers\parseYaml;
 
@@ -10,21 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 final class DifferTest extends TestCase
 {
-    // Проверка на примитивных типах
-    public function testPrimitiveTypes()
-    {
-        $this->assertSame('123', stringify(123));
-        $this->assertSame('"abc"', stringify('abc'));
-        $this->assertSame('true', stringify(true));
-    }
-
-    // Проверка на "плоских" данных
-    // public function testFlatData()
-    // {
-    //     $flatData = array('key1' => 'value1', 'key2' => 'value2');
-    //     $expectedResult = "{\n key1: value1\n key2: value2\n}";
-    //     $this->assertSame($expectedResult, stringify($flatData));
-    // }
+    
 
     // Проверка на "вложенных" данных
 
@@ -43,8 +29,8 @@ final class DifferTest extends TestCase
     public function test3()
     {
 
-        $file1 = $this->parseYaml('file111.yaml'); // не выводить на экран а использовать значения в файле
-        $file2 = $this->parseYaml('file222.yaml');  // не выводить на экран а использовать значения в файле
+        $file1 = parseYaml('file111.yaml'); // не выводить на экран а использовать значения в файле
+        $file2 = parseYaml('file222.yaml');  // не выводить на экран а использовать значения в файле
         $expected = file_get_contents($this->getPathToFile('jsonExpected')); // исправить ... сделать ожидаемое значение - файлом со значением. Или массивом
         $actual = genDiff($file1, $file2); // использовать значения из файлов и что то с ними сделать... например соеденить в один массив
         $message = "actual value for function test2() is not equals to expected"; // в сообщении можно определить конкретно данный тест для удобства работы с ошибками
